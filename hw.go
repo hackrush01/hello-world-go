e69681cf076f0d9366bb4a27ac82e3d85fe4ad87
hackrush <hackrush01@users.noreply.github.com>
2024-05-08 21:45:14 +0530

    Update hw.go

--- hw.go
+++ hw.go
@@ -7,14 +7,4 @@ import "flag"
 
 func main() {
     fmt.Println("Hello World!")
-
-    filePath := flag.String("filePath", "", "path of the json report")
-    flag.Parse()
-
-    data, err := os.ReadFile(*filePath)
-    if err != nil {
-        fmt.Printf("os.ReadFile(%s): %v\n", *filePath, err)
-        os.Exit(1)
-    }
-    fmt.Println(string(data))
 }
